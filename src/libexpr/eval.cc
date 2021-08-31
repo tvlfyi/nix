26e46f2d5d32aaf950a27710c463283352cdb12d
Taeer Bar-Yam <taeer@bar-yam.me>
2021-08-31 08:02:04 -0400

    refactor(3p/cppnix/eval): canonizePath -> canonicalizePath
    From <https://github.com/NixOS/nix/pull/5066>.
    
    Change-Id: I165a6a8301363fca0e25162786914ef989032e62
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/13923
    Autosubmit: sterni <sternenseemann@systemli.org>
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <tazjin@tvl.su>

--- src/libexpr/eval.cc
+++ src/libexpr/eval.cc
@@ -1589,7 +1589,7 @@ std::optional<string> EvalState::tryAttrsToString(const Pos & pos, Value & v,
 }
 
 string EvalState::coerceToString(const Pos & pos, Value & v, PathSet & context,
-    bool coerceMore, bool copyToStore, bool canonizePath)
+    bool coerceMore, bool copyToStore, bool canonicalizePath)
 {
     forceValue(v);
 
@@ -1601,7 +1601,7 @@ string EvalState::coerceToString(const Pos & pos, Value & v, PathSet & context,
     }
 
     if (v.type == tPath) {
-        Path path(canonizePath ? canonPath(v.path) : v.path);
+        Path path(canonicalizePath ? canonPath(v.path) : v.path);
         return copyToStore ? copyPathToStore(context, path) : path;
     }
 
