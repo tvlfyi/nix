063268fc66e04bb11d2417e6c2ba0d255e75ed34
Vincent Ambo <tazjin@tvl.su>
2025-02-09 19:48:53 +0000

    fix(libstore): avoid leaking user lock when failing to acquire file lock
    This (not exactly pretty) logic would previously lose track of the inserted user lock path, and not re-lock this path, when actually acquiring the lock through `lockFile` failed.
    
    In general this all seems off, and should be refactored. The two layers of locking are not fun to deal with.

--- src/libstore/build.cc
+++ src/libstore/build.cc
@@ -545,9 +545,10 @@ bool UserLock::findFreeUser() {
 
     /* Get the members of the build-users-group. */
     struct group * gr = getgrnam(settings.buildUsersGroup.get().c_str());
-    if (!gr)
+    if (!gr) {
         throw Error(format("the group '%1%' specified in 'build-users-group' does not exist")
             % settings.buildUsersGroup);
+    }
     gid = gr->gr_gid;
 
     /* Copy the result of getgrnam. */
@@ -557,65 +558,77 @@ bool UserLock::findFreeUser() {
         users.push_back(*p);
     }
 
-    if (users.empty())
+    if (users.empty()) {
         throw Error(format("the build users group '%1%' has no members")
             % settings.buildUsersGroup);
+    }
 
     /* Find a user account that isn't currently in use for another
        build. */
     for (auto & i : users) {
         debug(format("trying user '%1%'") % i);
 
-        struct passwd * pw = getpwnam(i.c_str());
-        if (!pw)
+        struct passwd* pw = getpwnam(i.c_str());
+        if (!pw) {
             throw Error(format("the user '%1%' in the group '%2%' does not exist")
                 % i % settings.buildUsersGroup);
-
+        }
 
         fnUserLock = (format("%1%/userpool/%2%") % settings.nixStateDir % pw->pw_uid).str();
 
+        bool inserted = false;
         {
             auto lockedPaths(lockedPaths_.lock());
-            if (lockedPaths->count(fnUserLock))
+            if (lockedPaths->count(fnUserLock)) {
                 /* We already have a lock on this one. */
                 continue;
+            }
             lockedPaths->insert(fnUserLock);
+            inserted = true;
         }
 
         try {
-
             AutoCloseFD fd = open(fnUserLock.c_str(), O_RDWR | O_CREAT | O_CLOEXEC, 0600);
-            if (!fd)
+            if (!fd) {
                 throw SysError(format("opening user lock '%1%'") % fnUserLock);
-
-            if (lockFile(fd.get(), ltWrite, false)) {
-                fdUserLock = std::move(fd);
-                user = i;
-                uid = pw->pw_uid;
-
-                /* Sanity check... */
-                if (uid == getuid() || uid == geteuid())
-                    throw Error(format("the Nix user should not be a member of '%1%'")
-                        % settings.buildUsersGroup);
-
-#if __linux__
-                /* Get the list of supplementary groups of this build user.  This
-                   is usually either empty or contains a group such as "kvm".  */
-                supplementaryGIDs.resize(10);
-                int ngroups = supplementaryGIDs.size();
-                int err = getgrouplist(pw->pw_name, pw->pw_gid,
-                    supplementaryGIDs.data(), &ngroups);
-                if (err == -1)
-                    throw Error(format("failed to get list of supplementary groups for '%1%'") % pw->pw_name);
-
-                supplementaryGIDs.resize(ngroups);
-#endif
-
-                return true;
             }
 
+            if (!lockFile(fd.get(), ltWrite, false)) {
+                if (inserted) {
+                    lockedPaths_.lock()->erase(fnUserLock);
+                    continue;
+                }
+            }
+
+            fdUserLock = std::move(fd);
+            user = i;
+            uid = pw->pw_uid;
+
+            /* Sanity check... */
+            if (uid == getuid() || uid == geteuid()) {
+                throw Error(format("the Nix user should not be a member of '%1%'")
+                            % settings.buildUsersGroup);
+            }
+
+#if __linux__
+            /* Get the list of supplementary groups of this build user.  This
+               is usually either empty or contains a group such as "kvm".  */
+            supplementaryGIDs.resize(10);
+            int ngroups = supplementaryGIDs.size();
+            int err = getgrouplist(pw->pw_name, pw->pw_gid, supplementaryGIDs.data(), &ngroups);
+            if (err == -1) {
+                throw Error(format("failed to get list of supplementary groups for '%1%'") % pw->pw_name);
+            }
+
+            supplementaryGIDs.resize(ngroups);
+#endif
+
+            return true;
         } catch (...) {
-            lockedPaths_.lock()->erase(fnUserLock);
+            if (inserted) {
+                lockedPaths_.lock()->erase(fnUserLock);
+            }
+
             return false;
         }
     }
