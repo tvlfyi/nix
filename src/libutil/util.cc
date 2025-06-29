ffc2e4d4aa1a5ff37a944d47765e4b40abd824ef
squalus <squalus@squalus.net>
2025-06-29 09:30:32 +0000

    fix(3p/cppnix): Improve durability of schema version file writes
    - call close explicitly in writeFile to prevent the close exception
      from being ignored
    - fsync after writing schema file to flush data to disk
    - fsync schema file parent to flush metadata to disk
    
    https: //github.com/NixOS/nix/issues/7064
    Change-Id: I55f031c1436a4bfa00cefd7788ddce7d4b0bb18c
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/13863
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI
    Autosubmit: sterni <sternenseemann@systemli.org>

--- src/libutil/util.cc
+++ src/libutil/util.cc
@@ -333,16 +333,22 @@ void readFile(const Path & path, Sink & sink)
 }
 
 
-void writeFile(const Path & path, const string & s, mode_t mode)
+void writeFile(const Path & path, const string & s, mode_t mode, bool sync)
 {
     AutoCloseFD fd = open(path.c_str(), O_WRONLY | O_TRUNC | O_CREAT | O_CLOEXEC, mode);
     if (!fd)
         throw SysError(format("opening file '%1%'") % path);
     writeFull(fd.get(), s);
+    if (sync)
+        fd.fsync();
+    // Explicitly close to make sure exceptions are propagated.
+    fd.close();
+    if (sync)
+        syncParent(path);
 }
 
 
-void writeFile(const Path & path, Source & source, mode_t mode)
+void writeFile(const Path & path, Source & source, mode_t mode, bool sync)
 {
     AutoCloseFD fd = open(path.c_str(), O_WRONLY | O_TRUNC | O_CREAT | O_CLOEXEC, mode);
     if (!fd)
@@ -356,8 +362,22 @@ void writeFile(const Path & path, Source & source, mode_t mode)
             writeFull(fd.get(), (unsigned char *) buf.data(), n);
         } catch (EndOfFile &) { break; }
     }
+
+    if (sync)
+        fd.fsync();
+    // Explicitly close to make sure exceptions are propagated.
+    fd.close();
+    if (sync)
+        syncParent(path);
 }
 
+void syncParent(const Path & path)
+{
+    AutoCloseFD fd = open(dirOf(path).c_str(), O_RDONLY, 0);
+    if (!fd)
+        throw SysError("opening file '%1%'", path);
+    fd.fsync();
+}
 
 string readLine(int fd)
 {
@@ -780,6 +800,19 @@ void AutoCloseFD::close()
     }
 }
 
+void AutoCloseFD::fsync()
+{
+  if (fd != -1) {
+      int result;
+#if __APPLE__
+      result = ::fcntl(fd, F_FULLFSYNC);
+#else
+      result = ::fsync(fd);
+#endif
+      if (result == -1)
+          throw SysError("fsync file descriptor %1%", fd);
+  }
+}
 
 AutoCloseFD::operator bool() const
 {
