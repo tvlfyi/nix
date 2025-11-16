6bd53cd00c6b8f5346f455ef57c1b6a71e1eb366
sterni <sternenseemann@systemli.org>
2025-11-16 15:54:13 +0100

    feat(3p/cppnix): export subtree to GitHub repository
    Change-Id: I67f51580876fc4176ff58d3deac443a5c5a896cb
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/13869
    Autosubmit: sterni <sternenseemann@systemli.org>
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <tazjin@tvl.su>

--- default.nix
+++ default.nix
@@ -1,5 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, depot, ... }:
 
 # The package is defined in ./package.nix, but since we want to inject it into
 # nixpkgs, //3p/overlays/tvl already calls it.
-pkgs.nix_2_3
+# Everything that requires depot needs to be addded here.
+pkgs.nix_2_3.overrideAttrs (oldAttrs: {
+  meta = oldAttrs.meta // {
+    ci = oldAttrs.meta.ci // {
+      extraSteps.export = depot.tools.releases.filteredGitPush {
+        filter = ":/third_party/cppnix";
+        remote = "git@github.com:tvlfyi/nix.git";
+        ref = "refs/heads/2.3-maintenance";
+      };
+    };
+  };
+})
